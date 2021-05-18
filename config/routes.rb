64741f6ba9d0a0ee8d68d012d6e331c080ba5d2b
Simon Debbarma <simonpeterdebbarma@outlook.com>
2021-05-18 16:21:59 +0100

    create post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :accounts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :communities
+  resources :communities do
+    resources :posts
+  end
 
   root to: "public#index"
 end
