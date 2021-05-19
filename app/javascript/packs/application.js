52adbcb941f5e289b53d5957b4cda89316b27bfd
Simon Debbarma <simonpeterdebbarma@outlook.com>
2021-05-19 11:12:11 +0100

    downgrade bootstrap, jquery, and popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("bootstrap")
-import "../stylesheets/application";
+import 'bootstrap/dist/js/bootstrap';
+import "../stylesheets/application"
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
@@ -17,3 +17,10 @@ document.addEventListener("turbolinks:load", () => {
   $('.toast').toast({ autohide: false })
   $('#toast').toast('show')
 })
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,15 @@
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+const { environment } = require('@rails/webpacker');
+const customConfig = require('./custom');
+const webpack = require('webpack');
 
-environment.plugins.prepend(
-    'Provide',
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      jquery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default'],
-    })
-)
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
+environment.config.merge(customConfig);
 
-module.exports = environment
+module.exports = environment;
